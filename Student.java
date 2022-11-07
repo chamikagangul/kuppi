e92e56831c3938f6bb9dcdfc4e5c203b8f4c5730
chamikagangul <pchamikagangul@gmail.com>
2022-11-07 21:30:56 +0530

    add result property

--- Student.java
+++ Student.java
@@ -2,10 +2,12 @@ class Student{
     private String name;
     private int age;
     private String school;
+    private String result;
     
     public Student(String name, int age,String school){
         this.name = name;
         this.age = age;
         this.school = school
     }
+
 }
