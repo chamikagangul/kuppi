d70b206418c39d2298fe6c00aa0a98c45d21fb21
chamikagangul <pchamikagangul@gmail.com>
2022-11-07 21:26:06 +0530

    add school property

--- Student.java
+++ Student.java
@@ -1,9 +1,11 @@
 class Student{
     private String name;
     private int age;
+    private String school;
     
-    public Student(String name, int age){
+    public Student(String name, int age,String school){
         this.name = name;
         this.age = age;
+        this.school = school
     }
 }
